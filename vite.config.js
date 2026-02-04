9eccb2b6d02de3c58d3a64570494cb7ef63d5e0c
Diego <diegoneves2021123@gmail.com>
2026-02-04 19:38:27 -0300

    atualização

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/Cheat-stopped-2.0/',
+  build: {
+    outDir: 'dist'
+  },
+  base: '/',
   server: {
     port: 5173,
     open: true
