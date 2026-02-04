2a3d98dbd41e8f3b73765e610dae333f7af10b12
Diego <diegoneves2021123@gmail.com>
2026-02-04 19:20:27 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+
+
+   theme: {
+    extend: {
+      colors: {
+        text: '#000000',
+        background: '#ffffff',
+        primary: '#ff0000',
+        secondary: '#ffffff',
+        accent: '#000000',
+      },
+    },
+  },
+  
+
+
+
+  plugins: [],
+}
+
